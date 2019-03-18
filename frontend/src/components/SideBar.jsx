e798c571cd21a3a50a2db524e2715585988257fd
hiufungk <hiufungk@ualberta.ca>
2019-03-18 03:55:00 -0400

    changed logout button to route to login page

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -82,7 +82,7 @@ class SideBar extends Component {
                         <i className="globe icon"/>
                         <Text>Public</Text>
                     </Nav>
-                    <Nav id="cc">
+                    <Nav id="">
                         <i className="sign-out icon"/>
                         <Text>logout</Text>
                     </Nav>
