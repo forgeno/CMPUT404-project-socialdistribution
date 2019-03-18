76481ce19018be25a82f9036bf3f11efdd2e9202
hiufungk <hiufungk@ualberta.ca>
2019-03-18 01:01:46 -0400

    3rd time doing this, seems to be working need to fix: css, author page when click on nav bar

---
+++ frontend/src/components/HigherOrder/withAuth.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import SideBar from "../SideBar";
+import {AppContainer as BaseAppContainer, ExampleBody as Body} from "../../containers";
+import {Redirect} from "react-router-dom";
+import styled from "styled-components";
+import Cookies from 'js-cookie';
+import store from '../../store/index.js';
+
+const AppContainer = styled(BaseAppContainer)`
+  height: calc(100vh - 40px);
+`;
+
+export default function withAuth(Component, navId) {
+    return class extends Component {
+        constructor(props) {
+            super(props);
+        }
+
+        // shouldComponentUpdate(nextState) {
+        //     return false;
+        // }
+
+
+        render() {
+            let isLoggedIn = store.getState().loginReducers.isLoggedIn || Cookies.get("userPass");
+            if (isLoggedIn) {
+                return (
+                    <AppContainer>
+
+                        <SideBar navId={navId}/>
+                        <Body>
+                        <Component {...this.props} />
+                        </Body>
+
+                    </AppContainer>
+                );
+            } else {
+                return (
+                    <Redirect to={"/"}/>
+                );
+            }
+        }
+    };
+}
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -3,58 +3,116 @@ import 'semantic-ui-css/semantic.min.css';
 import {Link} from "react-router-dom";
 import './styles/SideBar.css';
 import store from "../store/index";
+import { withRR4 } from "react-sidenav/withRR4";
+import { Nav as BaseNav } from "react-sidenav";
+import styled from "styled-components";
+import {
+  AppContainer as BaseAppContainer,
+  ExampleNavigation as BaseNavigation,
+  ExampleBody as Body
+} from "../containers";
+
+import {Tab} from 'semantic-ui-react';
 import utils from "../util/utils";
+
+
+const AppContainer = styled(BaseAppContainer)`
+  height: calc(100vh);
+  width: 100vw;
+`;
+
+const Navigation = styled(BaseNavigation)`
+  background: #303641;
+  color: #8d97ad;
+  font-size: 1em;
+  letter-spacing: 2px;
+  width: 100px;
+  line-height: 22px;
+`;
+
+const IconCnt = styled.div`
+  color: #FFF;
+  display: flex;
+  justify-content: center;
+  aligh-items: center;
+`;
+
+const Nav = styled(BaseNav)`
+  flex-direction: column;
+`;
+
+const theme = {
+  selectionColor: "#FFF",
+  hoverBgColor: "#181b20",
+  selectionBgColor: "#00BCD4"
+};
+
+const Text = styled.div`
+  font-size: 0.72em;
+  text-transform: uppercase;
+`;
+
+const SideNav = withRR4();
 
 class SideBar extends Component {	
 
 	constructor(props) {
 		super(props);
 		this.state = {
+			selectedPath: "2"
 		};
 	}
 
+	onItemSelection = arg => {
+		// this.setState({ selectedPath: arg.path });
+		console.log("on item select");
+  };
+
 	render() {
-		if(window.location.pathname !== "/") {
-			return(
-					<div className="ui left fixed vertical inverted sidebar labeled icon menu blue visible sideBarMenu">						
-						<Link to={{
-							  pathname: "/author/" + utils.getStripedEscapedAuthorId(store.getState().loginReducers.userId),
-							  state: {
-							  	fullAuthorId: store.getState().loginReducers.userId
-							  }
-							}} className="item sideBarProfile"
-						>
-							<span className={"ui circular tiny bordered centered image"}>						
-								<img alt="It's you!" src={require('../assets/images/default.png')}/>
-							</span>
-						  	<figcaption>Profile</figcaption>
-						</Link>
-
-						<Link to={"/stream"} className="item sideBarItem">
-							<i className="tint icon"/>
-						  	Stream
-						</Link>
-
-						<Link to="/friends" className="item sideBarItem">
-							<i className="users icon"/>
-						 	 Friends
-						</Link>
-
-						<Link to="/public" className="item sideBarItem">
-							<i className="globe icon"/>
-						  	Public
-						</Link>
-
-						<Link to="/" className="item sideBarItem">
-							<i className="sign-out icon"/>
-						  	Logout
-						</Link>
-					</div>
-			)
+		console.log(this.state);
+		let profilePath = "",
+			fullAuthorId = "";
+		if(store.getState().loginReducers.userId) {
+			profilePath = "author/" + utils.getStripedEscapedAuthorId(store.getState().loginReducers.userId);
+			fullAuthorId = store.getState().loginReducers.userId;
 		}
-		else {
-			return (<div>{null}</div>)
-		}
+
+
+		return(
+			<Navigation>
+          <SideNav
+			  basePath='/'
+            defaultSelectedPath="stream"
+            theme={theme}
+            onItemSelection={this.onItemSelection}
+          >
+		  	<Nav
+				id={profilePath}
+			  	payload={{
+					fullAuthorId: fullAuthorId
+			  	}}
+				className="item sideBarProfile"
+			>
+              <Text>Profile</Text>
+            </Nav>
+            <Nav id="stream">
+              <Text>Streamx</Text>
+            </Nav>
+            <Nav id="friends">
+              <Text>Friends</Text>
+            </Nav>
+		  	<Nav id="public">
+              <Text>Public</Text>
+            </Nav>
+			  <Nav id="bb">
+              <Text>test link</Text>
+            </Nav>
+			  <Nav id="cc">
+              <Text>logout</Text>
+            </Nav>
+          </SideNav>
+        </Navigation>
+		);
 	}
 }
 
