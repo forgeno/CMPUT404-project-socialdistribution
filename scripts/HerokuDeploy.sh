2c4c6f11b6116f7bef28834f48a8db2e383cc699
htruong1 <htruong1@ualberta.ca>
2019-03-19 19:55:04 -0600

    Redeployment scripts done

---
+++ scripts/HerokuDeploy.sh
@@ -0,0 +1,14 @@
+cd ../
+heroku login
+cd backend
+git add -A
+git commit -a -m "Redeploying Back End to Heroku"
+git push heroku master
+
+cd ../
+cd frontend
+git add -A
+git commit -a -m "Redeploying Front End to Heroku"
+git push heroku master
+
+
