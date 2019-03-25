b0e01eab89b9a65cd972192f55344859c3687f91
htruong1 <htruong1@ualberta.ca>
2019-03-24 23:36:17 -0600

    added migrate command for deploying

--- scripts/HerokuDeploy.sh
+++ scripts/HerokuDeploy.sh
@@ -4,6 +4,7 @@ cd backend
 git add -A
 git commit -a -m "Redeploying Back End to Heroku"
 git push heroku master
+heroku run --app radiant-savannah-77591 python manage.py migrate
 
 cd ../
 cd frontend
