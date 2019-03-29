0494b17cf22d1ea1901b6ebd1bd4fcec825a61b3
shchau <shchau1@ualberta.ca>
2019-03-29 14:28:14 -0600

    renamed files and variables to be more reflective of what they are

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -4,7 +4,7 @@ import './styles/NotFound.css';
 const NotFound = () => {
     return (
         <div className="container">
-			<img className="hmm" alt="hmm" src={require('../assets/images/hmm.png')}/>
+			<img className="image404" alt="Not Found" src={require('../assets/images/404Image.png')}/>
         	<h1 className="text404"> 404 </h1>
         
         	<Message as="h2"
