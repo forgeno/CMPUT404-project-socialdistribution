fa5889f315772076d5342a7b792d878d38e8389d
hiufungk <hiufungk@ualberta.ca>
2019-04-04 02:51:51 -0400

    removing gzip cuz didnt work

--- backend/project_404_T5/settings.py
+++ backend/project_404_T5/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.gzip.GZipMiddleware",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
